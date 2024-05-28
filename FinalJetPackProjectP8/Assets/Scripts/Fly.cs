71c497df4e3ffdbe1390c7ff59c97247907d9139
Angel Samuel Espinoza <759412@student.dusd.net>
2024-05-28 15:27:03 -0700

    day 2
    made them in the sky

---
+++ FinalJetPackProjectP8/Assets/Scripts/Fly.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float upforce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private float GameController;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upforce));
+                anim.SetTrigger("Flap");
+            }
+        }
+
+    }
+
+    void OnCollisionEnter2D()
+    {
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameController.instance.BirdDied();
+    }
+}
